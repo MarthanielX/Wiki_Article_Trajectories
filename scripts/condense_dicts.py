2c99caed27d6aac6151b676d0a28356374dda08b
sauerbergn <sauerbergn@carleton.edu>
2020-04-01 15:17:34 -0500

    updated condense dicts since can't you dict unpacking with dict compehension

--- scripts/condense_dicts.py
+++ scripts/condense_dicts.py
@@ -4,12 +4,10 @@ classes = ('FA', 'GA', 'B', 'C', 'ST', 'SB')
 types = ('graph', 'revision')
 
 for type in types:
-    dicts = []
-    for class in classes:
-        with open('./data/{}_dictionary_{}.pkl'.format(type, class), 'rb') as f:
-             dicts.append(pickle.load(f))
-
-    full_dict = {**x for x in dicts}
+    full_dict = {}
+    for cl in classes:
+        with open('./data/{}_dictionary_{}.pkl'.format(type, cl), 'rb') as f:
+             full_dict.update(pickle.load(f))
 
     with open('./data/{}_dictionary_all.pkl'.format(type), 'wb') as f:
        pickle.dump(full_dict, f)
