e9af737a19fcfd9b0a55e4fd40082a11aacca27b
nathaniel <nathaniel@Jims-Air.lan>
2020-04-09 15:56:21 -0700

    added more stats to generate_df_from_dict.py and made a bit more user friendly

--- scripts/generate_df_from_dict.py
+++ scripts/generate_df_from_dict.py
@@ -16,57 +16,79 @@ import pandas as pd
 import pickle
 import math
 
+""" Original Network Stats """
+
+def diameter(g, w=None):
+  if (w == None):
+    return nx.algorithms.distance_measures.diameter
+  dicts = nx.algorithms.shortest_paths.weighted.all_pairs_dijkstra_path_length(g,weight=w).values()
+  return max([x for x in dicts.values()])
+
+def average_closeness(g, w=None):
+  return statistics.mean(nx.algorithms.centrality.closeness_centrality(g, weight=w).values())
+
+def average_clustering(g, w=None):
+  return statistics.mean(nx.algorithms.cluster.clustering(g, weight=w).values())
+
+def average_betweenness(g, w=None):
+  return statistics.mean(nx.networkx.algorithms.centrality.betweenness_centrality(g, weight=w).values())
+
+""" Network Stats 2 """
+density = nx.classes.function.density(g)
+
+radius = nx.algorithms.distance_measures.radius
+
+def average_eccentricity(g):
+  return statistics.mean( nx.algorithms.distance_measures.eccentricity(g).values() )
+
+def number_of_edges(g):
+  return len(g.edges)
+
+global_clustering = nx.algorithms.cluster.transitivity
+
 """# Data Frame Constrution"""
 
-diameter = nx.algorithms.distance_measures.diameter
+stat_functions = {
+  'diameter': diameter,
+  'closeness' : average_closeness,
+  'avg clustering' : average_clustering,
+  'betweenness' : average_betweenness,
+  'density' : density,
+  'radius' : radius,
+  'avg eccentricity' : average_eccentricity,
+  'm' : number_of_edges,
+  'global clustering' : global_clustering
+}
 
-def average_closeness(g):
-  return statistics.mean(nx.algorithms.centrality.closeness_centrality(g).values())
-
-def average_clustering(g):
-  return statistics.mean(nx.algorithms.cluster.clustering(g).values())
-
-def average_betweenness(g):
-  return statistics.mean(nx.networkx.algorithms.centrality.betweenness_centrality(g).values())
-
-def create_article_row(index):
+# cannot do anything that requires the revision history
+def create_article_row(index, stat_names, directed):
   title = titles[index]
   print(index, title)
-  revisions = revision_dict[title]
-  graph = graph_dict[title]
-  return (
-    title,
-    class_dict[title],
-    diameter(graph),
-    average_closeness(graph),
-    average_clustering(graph),
-    average_betweenness(graph),
-    len(revisions), #num edits
-    len(graph), #num editors
-    revisions[0]['size'] #article length in bytes
-  )
 
-def construct_dataframe(article_titles):
-  rows = [create_article_row(i) for i in range(len(article_titles))]
-  return pd.DataFrame(rows, columns = ['title', 'class', 'diameter', 'closeness',
-                                       'clustering', 'betweenness',
-                                       'edit count', 'editor count', 'article size']).set_index('title')
+  graph = graph_dict[title]
+  if not directed:
+    graph = graph.to_undirected()
+
+  return (title, *(stat_functions[stat](graph) for stat in stat_functions))
+
+def construct_dataframe(article_titles, stat_names, directed):
+  return pd.DataFrame(
+    [create_article_row(i, stat_names, directed) for i in range(len(article_titles))],
+    columns = ['title', *stat_names] ).set_index('title')
+
+""" Main Method Section """
 
 with open('../data/graph_dictionary_all.pkl', 'rb') as f:
   graph_dict = pickle.load(f)
-
-with open('../data/revision_dictionary_all.pkl', 'rb') as f:
-  revision_dict = pickle.load(f)
-
-with open('../data/class_dictionary.pkl', 'rb') as f:
-  class_dict = pickle.load(f)
 
 with open('../data/article_titles_all.pkl', 'rb') as f:
   class_lists = pickle.load(f)
 
 titles = [item for sublist in class_lists for item in sublist]
+stats = ['density', 'radius', 'avg eccentricity', 'm', 'global_clustering']
+dir = True
 
-df = construct_dataframe(titles)
+df = construct_dataframe(titles, stats, dir)
 
-with open('../data/df_directed_stats_original.pkl', 'wb') as f:
-   pickle.dump(df, f)
+with open('../data/df_directed_stats2.pkl', 'wb') as f:
+  pickle.dump(df, f)
